f071b800f319075298690f5592f001588fa54a69
JoaoAugusto1374 <tauajoaoaugustoce@gmail.com>
2025-09-18 13:45:44 -0300

    feat: conectando o banco

---
+++ backend/config.py
@@ -0,0 +1,9 @@
+import os
+
+class Config:
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        "DATABASE_URL",
+        "postgresql://doacoes_user:senha123@localhost:5432/doacoes_db"
+    )
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.getenv("SECRET_KEY", "dev_secret_key")
