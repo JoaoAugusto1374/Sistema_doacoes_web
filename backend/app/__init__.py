d2b87ff69e7b090e0a4d4286af08a0e6cb018452
JoaoAugusto1374 <tauajoaoaugustoce@gmail.com>
2025-09-21 18:41:34 -0300

    fix: atualizando inicialização

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from .extensions import db, migrate
+from .extensions import db, migrate, jwt
 from .models import User, Campaign, Donation
 from config import Config
 
@@ -9,6 +9,12 @@ def create_app():
 
     db.init_app(app)
     migrate.init_app(app, db)
+    jwt.init_app(app)
+
+    from .routes.campaigns import campaigns_bp
+    app.register_blueprint(campaigns_bp, url_prefix="/api/campaigns")
+    from .routes.auth import auth_bp
+    app.register_blueprint(auth_bp, url_prefix="/api/auth")
 
     @app.route("/")
     def index():
